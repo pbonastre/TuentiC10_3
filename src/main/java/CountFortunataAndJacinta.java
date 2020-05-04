0929b51ad9416f8119fcc01473f49e11a4c37c9d
Paula Bonastre <bonastrep@gmail.com>
2020-05-04 22:06:15 +0200

    change to add count of words

--- src/main/java/CountFortunataAndJacinta.java
+++ src/main/java/CountFortunataAndJacinta.java
@@ -17,30 +17,30 @@ import static java.util.stream.Collectors.toMap;
 public class CountFortunataAndJacinta {
 
     private static final String INFILENAME = "C:\\workspace\\TC10_3FortunataJacinta\\input\\testInput.txt";
-    private static String OUTFILENAME = "C:\\workspace\\TC10_3FortunataJacinta\\output\\outputChallenge2.txt";
+    private static String OUTFILENAME = "C:\\workspace\\TC10_3FortunataJacinta\\output\\outputChallenge3.txt";
     private static Scanner in;
     private static PrintWriter out;
+    private static int countCases = 1;
 
     // Stream<String> stream = Stream.of(text.toLowerCase().split("\\W+")).parallel();
 
     //Map<String, Long> wordFreq = stream.collect(Collectors.groupingBy(String::toString,Collectors.counting()));
 
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    public static void main(String[] args) throws IOException {
         HashMap<String, Long> countWords = new HashMap<String, Long>();
         in = new Scanner(new FileReader(INFILENAME));
         out = new PrintWriter(OUTFILENAME);
         int cases = Integer.parseInt(in.nextLine());
         int countCases = 1;
-        Path path = Paths.get("C:\\workspace\\TC10_3FortunataJacinta\\input\\testInput.txt");
+        Path path = Paths.get("C:\\workspace\\TC10_3FortunataJacinta\\input\\testInputC.txt");
         countWords = countEveryWord(path);
-        countWords.remove(String.valueOf(cases));
-        orderNumbers(countWords, countCases, out);
-        orderWords(countWords, countCases, out);
+        orderNumbers(countWords, out);
+        orderWords(countWords, out);
         in.close();
         out.close();
     }
 
-    private static void orderNumbers(HashMap<String, Long> countWords, int countCases, PrintWriter out) {
+    private static void orderNumbers(HashMap<String, Long> countWords, PrintWriter out) {
         HashMap<String, Long> hashmapNumbers = new HashMap<String, Long>(countWords);
         hashmapNumbers.entrySet().removeIf(entry -> !isNumeric((String)entry.getKey()));
 
@@ -56,12 +56,9 @@ public class CountFortunataAndJacinta {
         while(iterator.hasNext()){
            entry = iterator.next();
             out.println("Case #" + (countCases) + ": " + entry.getKey() + " #"+entry.getValue());
-            System.out.println("Case #" + (countCases) + ": " + entry.getKey() + " #"+entry.getValue());
+            //System.out.println("Case #" + (countCases) + ": " + entry.getKey() + " #"+entry.getValue());
             countCases++;
         }
-        //numbersSortMap.forEach((k, v) -> out.println("Case #" + (countCases + 1) + ": " + v));
-
-        //
     }
 
     public static boolean isNumeric(String str) {
@@ -73,15 +70,25 @@ public class CountFortunataAndJacinta {
         }
     }
 
-    private static void orderWords(HashMap<String, Long> countWords, int countCases, PrintWriter out) {
+    private static void orderWords(HashMap<String, Long> countWords, PrintWriter out) {
+        HashMap<String, Long> hashmapWords = new HashMap<String, Long>(countWords);
+        hashmapWords.entrySet().removeIf(entry -> isNumeric((String)entry.getKey()));
+        hashmapWords.entrySet().removeIf(entry -> entry.getKey().length()<3);
+        TreeMap<String, Long> wordsSortMap = new TreeMap (hashmapWords);
+        wordsSortMap.forEach((k, v) -> out.println("Case #" + (countCases++) + ": " + k +" " + v) );
     }
 
     private static HashMap<String, Long> countEveryWord(final Path tmpFile) throws IOException {
-        return Pattern.compile("\\W+").splitAsStream(new String(Files.readAllBytes(tmpFile), StandardCharsets.UTF_8).toLowerCase())
+        String content = new String ( Files.readAllBytes( tmpFile), StandardCharsets.UTF_8);
+        int firstIntro = content.indexOf("\n");
+        content= content.substring(firstIntro+1);
+
+        return Pattern.compile("\\W+").splitAsStream(content.toLowerCase())
                 .collect(Collectors.groupingBy(Function.<String>identity(), HashMap::new, Collectors.counting()));
     }
 
     private void countWordsStream(final Path tmpFile) throws IOException {
+
         Arrays.stream(new String(Files.readAllBytes(tmpFile), StandardCharsets.UTF_8).split("\\W+"))
                 .collect(Collectors.groupingBy(Function.<String>identity(), HashMap::new, Collectors.counting()));
     }
